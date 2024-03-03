a8006f3d27d4e8ec46336395dc8e10eeb84e7eaf
Anderground <kvazar098543@gmail.com>
2024-03-03 13:21:02 +0200

    aa

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+a = int(input("Введите первое число:"))
+b = int(input("Введите второе число:"))
+c = int(input("Введите третье число:"))
+
+print(f"{a} + {b} + {c} = {a + b + c}")
+print(f"{a} * {b} * {c} = {a * b * c}")
+print(f"{a} / {b} / {c} = {a / b / c}")
+print(f"{a} - {b} - {c} = {a - b - c}")
