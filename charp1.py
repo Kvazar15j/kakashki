c4a53c09ced3f0c61ce99df2c088142b379c9e2d
Anderground <kvazar098543@gmail.com>
2024-03-17 11:28:22 +0200

    ого

--- charp1.py
+++ charp1.py
@@ -1,23 +1,37 @@
 class Character:
-    name = ""
-    hp = 100
+    name = ''
+    health = 100
     damage = 1
     defence = 0
 
-    def __init__(self, name, hp=100, damage=1, defence=0):
+    def __init__(self, name, health=100, damage=1, defence=0):
         self.name = name
-        self.hp = hp
+        self.health = health
         self.damage = damage
         self.defence = defence
 
     def show_info(self):
-        print(f' = {self.name} =\n'
-              f'Health: {self.hp} =\n'
-              f'Damage: {self.damage} =\n'
-              f'Defence: {self.defence} =\n')
+        print(self.__str__())
+
+    def __str__(self):
+        return f' = {self.name} =\n' \
+               f' Health: {self.health}\n' \
+               f' Damage: {self.damage}\n' \
+               f' Defence: {self.defence}\n' \
+
     def take_damage(self, damage):
-        self.hp -= damage
+        self.health -= damage
         return damage
 
     def attack(self, target):
         return target.take_damage(self.damage)
+
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+
+
+
+
--- charp2.py
+++ charp2.py
@@ -1,8 +1,19 @@
 from charp1 import Character
 
-player1 = Character("Anton")
+
+player1 = Character('Pudge')
 player1.show_info()
 
-
-player2 = Character("Lampochka", damage=2)
+player2 = Character('Tidehunter', damage=2)
 print(player2)
+
+p1_damage = player1.attack(player2)
+print(f'{player1.name} атакував {player2.name} '
+      f'і наніс {p1_damage} шкоди.')
+p2_damage = player2.attack(player1)
+print(f'{player2.name} атакував {player1.name} '
+      f'і наніс {p2_damage} шкоди.')
+
+print(player1, player2, sep='\n')
+
+
